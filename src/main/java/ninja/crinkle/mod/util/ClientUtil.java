af6810171b4c3474ddfd1ffc2209ea9413ac09ac
Galen <fuzzy.little.devil@gmail.com>
2023-11-22 12:37:37 -0500

    Dedicated Server fix, fix CI?

--- src/main/java/ninja/crinkle/mod/util/ClientUtil.java
+++ src/main/java/ninja/crinkle/mod/util/ClientUtil.java
@@ -1,6 +1,6 @@
 package ninja.crinkle.mod.util;
 
-import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.DistExecutor;
 import ninja.crinkle.mod.client.ClientHooks;
@@ -8,7 +8,7 @@ import ninja.crinkle.mod.client.ClientHooks;
 import java.util.Optional;
 
 public class ClientUtil {
-    public static LocalPlayer getPlayer() {
+    public static Player getPlayer() {
         return Optional.ofNullable(DistExecutor.safeCallWhenOn(Dist.CLIENT, () -> ClientHooks::getMinecraft))
                 .map(minecraft -> minecraft.player).orElse(null);
     }
